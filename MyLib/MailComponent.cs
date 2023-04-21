d0cd2c511b4968a15dce32062c7c3cba536f45a5
Alien663 <rite2844@gmail.com>
2023-04-21 17:30:50 +0800

    feat:Add Mail Component and Test

---
+++ MyLib/MailComponent.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Net.Mime;
+
+
+
+namespace MyLib
+{
+    public class MailComponent
+    {
+        private string SMTPServer;
+        private List<Attachment> Attachments = new List<Attachment>();
+        private MailMessage mail = new MailMessage();
+
+        public MailComponent(string SMTPServer, string Sender)
+        {
+            this.SMTPServer = SMTPServer;
+            this.mail.From = new MailAddress(Sender);
+        }
+
+        public void SendMail(string title, string context, bool isHTML = true)
+        {
+            #if DEBUG
+            title = "[Test]" + title;
+            context = "It's a test mail" + context;
+            #endif
+            this.mail.Subject = title;
+            this.mail.Body = context;
+            this.mail.IsBodyHtml = isHTML;
+            SmtpClient smtp = new SmtpClient(SMTPServer);
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtp.Send(this.mail);
+        }
+        public void SetReceiver(string receiver)
+        {
+            this.mail.To.Add(receiver);
+        }
+        public void SetReceiver(string[] receivers)
+        {
+            foreach(string receiver in receivers)
+            {
+                this.SetReceiver(receiver);
+            }
+        }
+        public void SetCC(string cc)
+        {
+            this.mail.CC.Add(cc);
+        }
+        public void SetCC(string[] ccs)
+        {
+            foreach(string cc in ccs)
+            {
+                this.SetCC(cc);
+            }
+        }
+        public void SetPicture(string context, string ID, string filepath, string mime)
+        {
+            AlternateView htmlview = AlternateView.CreateAlternateViewFromString(context, null, MediaTypeNames.Text.Html);
+            LinkedResource imageLink = new LinkedResource(filepath, mime);
+            imageLink.ContentId = ID;
+            imageLink.TransferEncoding = TransferEncoding.Base64;
+            htmlview.LinkedResources.Add(imageLink);
+            this.mail.AlternateViews.Add(htmlview);
+        }
+        public void SetAttachment(string filepath)
+        {
+            this.mail.Attachments.Add(new Attachment(filepath));
+        }
+        public void SetAttachment(string[] filepaths)
+        {
+            foreach(string filepath in filepaths)
+            {
+                this.SetAttachment(filepath);
+            }
+        }
+        public void SetAttachment(Attachment attachment)
+        {
+            this.mail.Attachments.Add(attachment);
+        }
+        public void SetAttachment(List<Attachment> attachments)
+        {
+            foreach(Attachment attachment in attachments)
+            {
+                this.SetAttachment(attachment);
+            }
+        }
+    }
+}
---
+++ TestProject1/MailTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyLib;
+using NUnit.Framework;
+
+namespace TestMyLib
+{
+    public class MailTest
+    {
+        [SetUp]
+        private void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test()
+        {
+            string context = @"
+<img src='cid:logo'></img><br>
+Test Send Mail<br>
+";
+            MailComponent _mail = new MailComponent("SMTP Server", "Sender");
+            _mail.SetReceiver("Mail Receiver");
+            _mail.SetCC("CC to some one");
+            _mail.SetPicture(context, "logo", "picture path", "memetype, ex: img/png");
+            _mail.SetAttachment("file path");
+            _mail.SendMail("Test Mail", context);
+        }
+    }
+}
