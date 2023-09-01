ce31454209784790e8b74468785ebc7968e2014a
11107063 <11107063@AN990154054>
2023-09-01 15:59:25 +0800

    the first commit

---
+++ TestProject1/DataExtensions.cs
@@ -0,0 +1,62 @@
+﻿using MathNet.Numerics.Distributions;
+using MyLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using NUnit.Framework;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Org.BouncyCastle.Asn1.BC;
+
+namespace TestMyLib
+{
+    public class TestDataExtensions
+    {
+        private DataTable dtStudent = new DataTable();
+        private List<Student> dmStudent = new List<Student>();
+        [SetUp]
+        public void Setup()
+        {
+            dtStudent.Columns.AddRange(new DataColumn[3] { new DataColumn("Name"), new DataColumn("StudentId"), new DataColumn("Age") });
+            dtStudent.Rows.Add("Jack", 15, 100);
+            dtStudent.Rows.Add("Smith", 17, 101);
+            dtStudent.Rows.Add("Karoro", 20, 102);
+
+            dmStudent.Add(new Student() { Name = "Jack", Age = 15, StudentId = 100 });
+            dmStudent.Add(new Student() { Name = "Smith", Age = 17, StudentId = 101 });
+            dmStudent.Add(new Student() { Name = "Karoro", Age = 20, StudentId = 102 });
+        }
+
+        [Test]
+        public void Table2Model()
+        {
+            List<Student> dmData = (List<Student>)dtStudent.ToList<Student>();
+            if(dmData.Count> 0)
+            {
+                Assert.Pass("DataModel transfer success");
+            }
+            else
+            {
+                Assert.Fail("DataModel is empty");
+            }
+        }
+
+
+        [Test]
+        public void Model2Table()
+        {
+            DataModelExtensions dmConvertor = new DataModelExtensions();
+            DataTable dtData = dmConvertor.ToDataTable(dmStudent);
+            if (dtData.Rows.Count > 0)
+            {
+                Assert.Pass("DataModel transfer success");
+            }
+            else
+            {
+                Assert.Fail("DataModel is empty");
+            }
+        }
+    }
+}
---
+++ TestProject1/ExcelComponent.cs
@@ -0,0 +1,126 @@
+using System;
+using System.Linq;
+using System.IO;
+using System.Data;
+using System.Reflection;
+using System.Collections.Generic;
+using NUnit.Framework;
+using MyLib;
+using System.ComponentModel;
+
+namespace TestMyLib
+{
+    public class TestExportExcel
+    {
+        private DataTable dtStudent = new DataTable();
+        private readonly string folder = @"D:\Test\";
+        private List<Student> Students = new List<Student>();
+        [SetUp]
+        public void Setup()
+        {
+            DataModelExtensions dmConvertor = new DataModelExtensions();
+            Students.Add(new Student() { Name = "Jack", Age = 15, StudentId = 100 });
+            Students.Add(new Student() { Name = "Smith", Age = 17, StudentId = 101 });
+            Students.Add(new Student() { Name = "Karoro", Age = 20, StudentId = 102 });
+            dtStudent = dmConvertor.ToDataTable(Students);
+        }
+
+        [Test]
+        public void DataTable2Excel()
+        {
+            ExcelComponent myexcel = new ExcelComponent();
+            byte[] data = myexcel.export(this.dtStudent);
+            using (FileStream fs = File.Create(this.folder + "test.xlsx"))
+            {
+                fs.Write(data, 0, data.Length);
+            }
+            Assert.Pass("DataTable export out excel file success");
+        }
+
+        [Test]
+        public void DataSet2Excel()
+        {
+            DataTable dtstudent2 = this.dtStudent.Copy();
+            dtstudent2.TableName = "hahaha";
+            DataSet st = new DataSet();
+            st.Tables.Add(this.dtStudent);
+            st.Tables.Add(dtstudent2);
+            ExcelComponent myexcel = new ExcelComponent();
+            byte[] data = myexcel.export(st);
+            using (FileStream fs = File.Create(this.folder + "test2.xlsx"))
+            {
+                fs.Write(data, 0, data.Length);
+            }
+            Assert.Pass(message:"DataSet export out excel file success");
+        }
+
+        [Test]
+        public void DataModel2Excel()
+        {
+            ExcelComponent myexcel = new ExcelComponent();
+            var data = myexcel.export(this.Students);
+            using (FileStream fs = File.Create(this.folder + "test3.xlsx"))
+            {
+                fs.Write(data, 0, data.Length);
+            }
+            Assert.Pass("DataModel export out excel file success");
+        }
+    }
+
+    public class TestReadFromExcel
+    {
+        private readonly string folder = @"D:\Test\";
+
+        [Test]
+        public void TestReadFile2DataTable()
+        {
+            string filepath = this.folder + "test.xlsx";
+            FileStream fs = new FileStream(filepath, FileMode.Open, FileAccess.Read);
+            ExcelComponent myexcel = new ExcelComponent();
+            DataTable dt = myexcel.readFileDT(fs);
+            if(dt.Rows.Count > 0)
+            {
+                Assert.Pass("It can read data from excel to DataTable");
+            }
+            else
+            {
+                Assert.Fail("DataTable is empty");
+            }
+        }
+
+        [Test]
+        public void TestReadFile2DataSet()
+        {
+            string filepath = this.folder + "test2.xlsx";
+            FileStream fs = new FileStream(filepath, FileMode.Open, FileAccess.Read);
+            ExcelComponent myexcel = new ExcelComponent();
+            DataSet ds = myexcel.readFileDS(fs);
+            if (ds.Tables.Count > 0)
+            {
+                Assert.Pass("It can read data from exlcel to DataSet");
+            }
+            else
+            {
+                Assert.Fail("DataSet is empty");
+            }
+            
+        }
+
+        [Test]
+        public void TestReadFile2DataModel()
+        {
+            string filepath = this.folder + "test.xlsx";
+            FileStream fs = new FileStream(filepath, FileMode.Open, FileAccess.Read);
+            ExcelComponent myexcel = new ExcelComponent();
+            List<Student> dm = myexcel.readFileDM<Student>(fs, 0, 0);
+            if(dm.Count > 0)
+            {
+                Assert.Pass("It can read data from excel to DataModel list");
+            }
+            else
+            {
+                Assert.Fail("DataModel is empty");
+            }
+        }
+    }
+}
