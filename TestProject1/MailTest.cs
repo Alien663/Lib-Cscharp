00b0e1c23ef9ddb1201f48f76c32123b373d598b
11107063 <11107063@AN990154054>
2023-09-12 12:56:47 +0800

    feat:Add Mail Unit Test

--- TestProject1/MailTest.cs
+++ TestProject1/MailTest.cs
@@ -1,5 +1,7 @@
 ﻿using NUnit.Framework;
 using SimpleMail;
+using System.Collections.Generic;
+using System.Net.Mail;
 
 namespace TestMyLib
 {
@@ -8,34 +10,49 @@ namespace TestMyLib
         [Test]
         public void Test1()
         {
-//            string context = @"
-//<img src='cid:logo'></img><br>
-//Test Send Mail<br>";
-//            MailComponent _mail = new MailComponent("SMTP Server", "Sender");
-//            _mail.setReceiver("Mail Receiver");
-//            _mail.setCC("CC to some one");
-//            _mail.setPicture(context, "logo", "picture path", "memetype, ex: img/png");
-//            _mail.setAttachment("file path");
-//            _mail.sendMail("Test Mail", context);
+            string context = @"
+Hi Leon,<br>
+Help me to do the unit test of mail module<br>
+<img src='cid:logo'></img><br>
+Test Send Mail<br>";
+            MailComponent _mail = new MailComponent("10.243.29.110", "tds@quantatw.com");
+            _mail.setReceiver ("Bryan_Lin@quantatw.com");
+            _mail.setCC("leon-chen@quantatw.com");
+            _mail.setPicture(context, "logo", @".\icon.png", "img/png");
+            _mail.setAttachment(@".\icon.png");
+            _mail.setAttachment(new string[] { @".\icon.png" });
+            _mail.sendMail("[Test] Unit Test Mail", context);
         }
 
         [Test]
         public void Test2()
         {
-//            string context = @"
-//<img src='cid:logo'></img><br>
-//Test Send Mail<br>";
-
-//            MailPictureModel _picture = new MailPictureModel
-//            {
-//                ID = "logo",
-//                FilePath = "picture path",
-//                Mime = "img/png",
-//            };
-//            MailComponent _mail = new MailComponent("SMTP Server", "Sender");
-//            _mail.setReceiver("Mail Receiver");
-//            _mail.setPicture(context, _picture);
-//            _mail.sendMail("Test Mail", context);
+            string context = @"
+Hi Leon,<br>
+Help me to do the unit test of mail module<br>
+<img src='cid:logo1'></img><br>
+<img src='cid:logo2'></img><br>
+Test Send Mail<br>";
+            MailComponent _mail = new MailComponent("10.243.29.110", "tds@quantatw.com");
+            _mail.setReceiver(new string[] { "Bryan_Lin@quantatw.com" });
+            _mail.setCC(new string[] { "leon-chen@quantatw.com" });
+            _mail.setPicture(context, new MailPictureModel
+            {
+                ID="logo1",
+                FilePath=@".\icon.png",
+                Mime="img/png"
+            });
+            _mail.setPicture(context, new List<MailPictureModel>
+            {
+                new MailPictureModel
+                {
+                    ID="logo2",
+                    FilePath=@".\icon.png",
+                    Mime="img/png"
+                }
+            });
+            _mail.setAttachment(new List<Attachment>{ new Attachment(@".\icon.png") });
+            _mail.sendMail("[Test] Unit Test Mail", context);
         }
     }
 }
