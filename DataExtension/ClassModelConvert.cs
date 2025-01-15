5ca30a0ce60aa2014a92324edd554a5fb7ec0ee2
11107063 <11107063@AN990154054>
2025-01-15 11:39:59 +0800

    🐛 Fix Unit Test Project

--- DataExtension/ClassModelConvert.cs
+++ DataExtension/ClassModelConvert.cs
@@ -10,18 +10,22 @@ namespace Data.Extension
 {
     public static class ClassModelConvert
     {
-        public static DataTable ToDataTable<T>(List<T> items)
+        public static DataTable ToDataTable<T>(List<T> items) where T : class
         {
             DataTable dataTable = new DataTable(typeof(T).Name);
             //Get all the properties
             PropertyInfo[] Props = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
             foreach (PropertyInfo prop in Props)
             {
+                Type propType = prop.PropertyType;
+                if(propType.IsGenericType && propType.GetGenericTypeDefinition() == typeof(Nullable<>))
+                {
+                    propType = Nullable.GetUnderlyingType(propType);
+                }
                 //Setting column names as Property names
                 dataTable.Columns.Add(prop.Name, prop.PropertyType);
             }
-            foreach (T item in items)
-            {
+            Parallel.ForEach(items, item => {
                 var values = new object[Props.Length];
                 for (int i = 0; i < Props.Length; i++)
                 {
@@ -29,7 +33,7 @@ namespace Data.Extension
                     values[i] = Props[i].GetValue(item, null);
                 }
                 dataTable.Rows.Add(values);
-            }
+            });
             //put a breakpoint here and check datatable
             return dataTable;
         }
